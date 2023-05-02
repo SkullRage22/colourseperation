fc431a7d812f3ad04cdc4ef504f6c551d30f940d
SkullRage22 <132365080+SkullRage22@users.noreply.github.com>
2023-05-02 19:29:24 +0530

    Create compute_sad_loss.m

---
+++ eval/compute_sad_loss.m
@@ -0,0 +1,8 @@
+% compute the SAD error given a prediction, a ground truth and a trimap.
+function loss = compute_sad_loss(pred,target,trimap)
+error_map = abs(single(pred)-single(target))/255;
+loss = sum(sum(error_map.*single(trimap==128))) ;
+
+% the loss is scaled by 1000 due to the large images used in our experiment.
+% Please check the result table in our paper to make sure the result is correct. 
+loss = loss / 1000 ;
